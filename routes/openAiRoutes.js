fccb5c99493e97f8034a18ed80d0db9914c309cf
Illia Meserenko <126924391+Meserenko@users.noreply.github.com>
2024-06-05 14:07:51 -0700

    Add files via upload

---
+++ routes/openAiRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { generateImage } = require('../controllers/openAiController');
+
+router.post('/generateimage', generateImage);
+
+module.exports = router;
